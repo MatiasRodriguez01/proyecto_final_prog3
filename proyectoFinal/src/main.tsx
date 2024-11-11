a6fcf10d5e5edc3f71104bf2e06479f0c818359a
Matias Rodriguez <matiasrodriguez3002@gmail.com>
2024-11-11 18:30:59 -0300

    se resolvieron los conflictos de main.tsx y appRouter.tsx

--- proyectoFinal/src/main.tsx
+++ proyectoFinal/src/main.tsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-<<<<<<< HEAD
-// import "@fontsource/material-symbols-outlined";
-import store from "../src/store/store"
 import { Provider } from "react-redux";
-=======
-
->>>>>>> de8d42904c482b8cbfeef0643525fc82d6c54d43
+import store from "./store/store";
 
 const link = document.createElement("link");
 link.href =
@@ -17,12 +12,8 @@ document.head.appendChild(link);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
     <Provider store={store}>
       <App/>
     </Provider>
-=======
-    <App />
->>>>>>> de8d42904c482b8cbfeef0643525fc82d6c54d43
   </StrictMode>
 );
