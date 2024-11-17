8df366a8b4d399d0f122240b06c07b638d932007
Matias Rodriguez <matiasrodriguez3002@gmail.com>
2024-11-17 19:43:24 -0300

    se agrego la carpeta de modal de editar y crear categoria, y lo mismo para subCategoria

--- proyectoFinal/src/components/views/vistaAdmin/ACategorias/ModalEditarSubCategoria/ModalEditarSubCategoria.tsx
+++ proyectoFinal/src/components/views/vistaAdmin/ACategorias/ModalEditarSubCategoria/ModalEditarSubCategoria.tsx
@@ -1,123 +1,125 @@
-import { FC, FormEvent } from "react";
-import { Modal, Button } from "react-bootstrap";
-import { ServiceCategorias } from "../../../../../services/ServiceCategorias";
-import { useForm } from "../../../../../hooks/useForm";
-import { IEmpresa } from "../../../../../types/dtos/empresa/IEmpresa";
+import { ChangeEvent, FC, useEffect } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+import { ICategorias } from '../../../../../types/dtos/categorias/ICategorias';
+import { ServiceCategorias } from '../../../../../services/ServiceCategorias';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../../store/store';
+import { IUpdateCategoria } from '../../../../../types/dtos/categorias/IUpdateCategoria';
+import { ISucursal } from '../../../../../types/dtos/sucursal/ISucursal';
+import { useForm } from '../../../../../hooks/useForm';
 
-import styles from "./ModalCrearCategoria.module.css";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../../store/store";
-import { IUpdateCategoria } from "../../../../../types/dtos/categorias/IUpdateCategoria";
+import stylesEditarSubCategoria from "./ModalEditarSubCategoria.module.css";
 
-interface ModalEditarSubcategoriaProps {
-  empresa: IEmpresa | null,
-  show: boolean;
-  onClose: () => void;
-  categoria?: any;
+interface IProsEditarCategoria {
+    show: boolean;
+    onClose: () => void;
+    categoria: ICategorias,
+    sucursales: ISucursal[]
 }
 
-const ModalEditarSubcategoria: FC<ModalEditarSubcategoriaProps> = ({ empresa, show, onClose }) => {
+export const ModalEditarSubCategoria: FC<IProsEditarCategoria> = ({ show, onClose, categoria, sucursales }) => {
 
-  // servicio de categoria
-  const serviceCategoria = new ServiceCategorias()
+    if (categoria !== null) {
+        //const dispatch = useDispatch<AppDispatch>();
+        const serviceCategoria = new ServiceCategorias();
 
-  // categoriaActiva del store
-  const categoriaActiva = useSelector((state: RootState) => state.categoria.categoriaActiva);
-  // ID de la categoria activa
-  const idCategoriaActiva: number = Number(categoriaActiva?.id);
-  // ID de la empresa 
-  const idEmpresa: number = Number(empresa?.id);
+        const empresaActiva = useSelector((state: RootState) => { return state.empresa.empresaActiva })
 
-  const { values, handleChange, resetForm } = useForm({
-    denominacion: "",
-    IdEmpresa: idEmpresa,
-    IdCategoriaPadre: idCategoriaActiva,
-  })
+        const { values, handleChange, resetForm, setValues } = useForm({
+            denominacion: categoria.denominacion || "",
+            idEmpresa: empresaActiva?.id || 0,
+            idCategoriaPadre: null
+        });
 
-  const handleCreateCategoria = async (categoria: IUpdateCategoria) => {
-    try {
-      const response = await serviceCategoria.editOneCategoria(idCategoriaActiva, categoria)
+        const sucursalIds = sucursales.map((sucursal) => sucursal.id)
 
-      console.log(response.id)
+        // Actualiza los valores del formulario si la empresa cambia
+        useEffect(() => {
+            if (categoria && show && empresaActiva) {
+                setValues({
+                    denominacion: categoria.denominacion,
+                    idEmpresa: empresaActiva.id,
+                    idCategoriaPadre: null
+                });
+            }
+        }, [categoria]);
 
-      console.log("ID de la subCategoria creada: ", categoria.idEmpresa)
-    } catch (error) {
-      console.error("Error al crear la subCategoria, ", error)
+        const handleEditarCategoria = async (categoriaEditar: IUpdateCategoria) => {
+            try {
+                await serviceCategoria.editOneCategoria(categoriaEditar.id, categoriaEditar)
+            } catch (error) {
+                console.error("Error editar Empresa: ", error)
+            }
+            //onAddEmpresa(newEmpresa);
+        };
+
+        const addForm = () => {
+            const newCategoria: IUpdateCategoria = {
+                id: categoria.id,
+                denominacion: values.denominacion,
+                eliminado: false,
+                idEmpresa: values.idEmpresa,
+                idSucursales: sucursalIds
+            };
+            handleEditarCategoria(newCategoria);
+            console.log(newCategoria.id)
+            resetForm(); // Cerrar el modal
+            onClose()
+        }
+        const cancelForm = () => {
+            resetForm();
+            onClose();
+        };
+
+        const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+            event.preventDefault();
+        };
+
+        // Si no está visible, no renderiza nada
+        if (!show) {
+            return null;
+        }
+
+        return (
+            <Modal show={show} onHide={onClose}>
+                <Modal.Header >
+                    <Modal.Title>Crear/Editar Categoría</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/* Formulario para crear categoría */}
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="categoryName"
+                                name="denominacion"
+                                value={values.denominacion}
+                                placeholder="Ingrese una denominacion"
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="primary"
+                        onClick={cancelForm}
+                        className={stylesEditarSubCategoria.botonCancelar}
+                    >
+                        Cancelar
+                    </Button>
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className={stylesEditarSubCategoria.botonAceptar}
+                        form="categoryForm"
+                        onClick={addForm}
+                    >
+                        Guardar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        )
     }
-  }
-
-  const addForm = () => {
-    if (empresa !== null) {
-      const newCategoria: IUpdateCategoria = {
-        id: 0,
-        eliminado: false,
-        idSucursales: [0, 1, 2],
-        denominacion: values.denominacion,
-        idCategoriaPadre: values.IdCategoriaPadre,
-        idEmpresa: empresa.id
-      }
-
-      handleCreateCategoria(newCategoria)
-      resetForm()
-      onClose()
-    }
-  }
-
-  const cancelForm = () => {
-    resetForm()
-    onClose()
-  }
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    addForm()
-  }
-
-  if (!show) {
-    return null;
-  }
-
-  return (
-    <Modal show={show} onHide={onClose}>
-      <Modal.Header >
-        <Modal.Title>Crear SubCategoria</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {/* Formulario para crear categoría */}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="categoryName"
-              name="denominacion"
-              value={values.denominacion}
-              placeholder="Ingrese una denominacion"
-              onChange={handleChange}
-            />
-          </div>
-        </form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="primary"
-          onClick={cancelForm}
-          className={styles.botonCancelar}
-        >
-          Cancelar
-        </Button>
-        <Button
-          type="submit"
-          variant="primary"
-          className={styles.botonAceptar}
-          form="categoryForm"
-          onClick={addForm}
-        >
-          Guardar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
-};
-
-export default ModalEditarSubcategoria;
+}
